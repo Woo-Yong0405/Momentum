886fa6c354c1b9f40ebd5f8f112c85d46e7eb68f
WooYong333 <81574524+WooYong333@users.noreply.github.com>
2021-08-10 10:47:11 +0900

    finished javascript and html

--- js/bg.js
+++ js/bg.js
@@ -2,4 +2,6 @@ const images = ["0.jpeg", "1.jpeg", "2.jpeg", "3.jpeg"];
 const chosenImg = images[Math.floor(Math.random() * images.length)];
 const bg = document.createElement("img");
 bg.src = `img/${chosenImg}`;
-document.body.appendChild(bg)
+document.body.appendChild(bg);
+const bh = document.querySelector("img");
+bh.classList.add("bgbg");
--- js/weather.js
+++ js/weather.js
@@ -1,9 +1,21 @@
-function onGeoSuccess() {
-    
+const APIkey = "1215b28b2f65830f3b022d654f16ec6a";
+
+function onGeoSuccess(position) {
+    const lat = position.coords.latitude;
+    const long = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${APIkey}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}°C`;
+        });
 }
 
 function onGeoErr() {
-    alert("Can't locate you")
+    alert("Cannot locate your current position")
 }
 
 navigator.geolocation.getCurrentPosition(onGeoSuccess, onGeoErr);
